b9cab481731767f980aa2e5958bff884f3d22ad4
Hardy Mansén <hardy@opn.ooo>
2023-05-29 17:38:32 +0700

    nvim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,11 @@
+-- Main Configuration
+require("config/packer")
+require("config/set")
+require("config/keymap")
+require("config/lsp")
+require("config/colors")
+
+-- Plugin Configuration
+require("plugin/telescope")
+require("plugin/fugitive")
+require("plugin/undotree")
