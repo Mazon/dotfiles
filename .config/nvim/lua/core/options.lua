b2d2fb6c1a54640a8750cae04d5dc1829482627b
mazon <info@nordiclight.org>
2024-03-20 23:12:50 +0700

    options

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -83,9 +83,6 @@ vim.g.python3_host_prog = '/Library/Frameworks/Python.framework/Versions/3.11/bi
 -- Markdown
 vim.g.markdown_fenced_languages = { 'html', 'python', 'lua', 'js=javascript' }
 
--- Greek langmap
--- stylua: ignore
-vim.opt.langmap = 'ΑA,ΒB,ΨC,ΔD,ΕE,ΦF,ΓG,ΗH,ΙI,ΞJ,ΚK,ΛL,ΜM,ΝN,ΟO,ΠP,QQ,ΡR,ΣS,ΤT,ΘU,ΩV,WW,ΧX,ΥY,ΖZ,αa,βb,ψc,δd,εe,φf,γg,ηh,ιi,ξj,κk,λl,μm,νn,οo,πp,qq,ρr,σs,τt,θu,ωv,ςw,χx,υy,ζz'
 
 -- Open telescope file_browser if nvim has no arguments
 -- if vim.fn.argc() == 0 then
